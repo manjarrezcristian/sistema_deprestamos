c864edd5cb77b465070133e56f71cdfa43e5b84e
manjarrezcristian <cristiantovarm@gmail.com>
2025-05-08 23:20:36 -0500

    producto semana 12

--- sistema_deprestamos/sistema_de_prestamos.java
+++ sistema_deprestamos/sistema_de_prestamos.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class sistema_de_prestamos {
     public static void main(String[] args) {
-Scanner leer =new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
 
         String fechaHoy, nombreEstudiante, documentoEstudiante, direccionEstudiante;
         String telefonoEstudiante, correoEstudiante, matriculado;
@@ -33,6 +33,7 @@ Scanner leer =new Scanner(System.in);
         // Condicional: validar si puede prestar
         if (!matriculado.equals("si")) {
             System.out.println("No puede acceder al préstamo si no está matriculado.");
+            System.out.println("Intregantes: Estefania Cataño, Carolina Bolivar, Luenyi, Cristian Tovar");
             return;
         }
 
@@ -51,10 +52,19 @@ Scanner leer =new Scanner(System.in);
         System.out.print("Ingrese la cantidad de días del préstamo: ");
         tiempoPrestamo = leer.nextInt();
 
-        if (tiempoPrestamo > 30) {
-            System.out.println("El tiempo máximo de préstamo es de 30 días.");
+        if (tiempoPrestamo > 7) {
+            System.out.println("El tiempo maximo de préstamo es de 7 días a partir de la fecha registrada.");
+            System.out.println("Intregantes: Estefania Cataño, Carolina Bolivar, Luenyi, Cristian Tovar");
         } else {
-            System.out.println("Préstamo registrado con éxito.");
+            System.out.println("\n--- RESUMEN DEL PRÉSTAMO ---");
+            System.out.println(
+                    "El día " + fechaHoy + ", el estudiante " + nombreEstudiante +
+                            " con documento: " + documentoEstudiante + ", domiciliado en " + direccionEstudiante +
+                            ", con teléfono " + telefonoEstudiante + " y correo " + correoEstudiante +
+                            ", solicitó el préstamo del libro \"" + tituloLibro + "\", escrito por " + autorLibro +
+                            ", publicado por la editorial " + editorialLibro + " con " + numPaginas + " páginas. "
+            );
+            System.out.println("Intregantes: Estefania Cataño, Carolina Bolivar, Lorena mejia, Cristian Tovar");
         }
     }
 }
